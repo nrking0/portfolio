ff9d7d68b921d9e5deb565e8c0faba5d51298ea4
Nick King <nrking0@gmail.com>
2020-09-27 02:58:11 -0500

    Added Sidebar with Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar/sidebar.js';
+import Home from './Components/Home/Home.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <Sidebar />
+      </div>
+      <div className="test">
+      <Home />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = (props) => {
+
+    return (
+        <div className="base">
+           <p>Hello, World!</p>
+        </div>
+    )
+};
+
+export default Home;
