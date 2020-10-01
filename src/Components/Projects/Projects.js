f3deb80fd24fc9e102989370bf17b9049270485b
Nick King <nrking0@gmail.com>
2020-09-30 23:16:51 -0500

    cleaned up css and buttons

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Projects.css';
 import { Element } from 'react-scroll';
 import { FormatAlignJustify } from '@material-ui/icons';
+import Button from '@material-ui/core/Button';
 
 const Projects = (props) => {
 
@@ -11,24 +12,25 @@ const Projects = (props) => {
             <section style={{margin: 'auto', justifyContent: 'center'}}class="main items">
                     <article style={{width: '40vh', marginBottom: '5%'}} class="item">
                         <header>
-                            <a href="https://voteuhigh.herokuapp.com"><img className="voteImage" src={require('../../Images/voteuhigh.png')} alt="" /></a>
+                            <a href="https://voteuhigh.herokuapp.com" target="_blank"><img className="voteImage" src={require('../../Images/voteuhigh.png')} alt="" /></a>
                             <h3>Vote U-High</h3>
                         </header>
                         <p>This is a project about... ajfhald;hfjakls;hlk;dasfh fkdlhdfjalk;dsfkl;sdfhkl ;dsfanjdsf kl;fdcvjndksla;jfkl ;sdnfkml,sdaf; nklas;nfkl;</p>
-
-                        <a href="https://voteuhigh.herokuapp.com" class="button">Learn More</a>
+                        <Button href="https://voteuhigh.herokuapp.com" variant="contained" color="primary" target="_blank">Visit Site</Button>
+                        
                     </article>
                 
                     <article style={{width: '40vh', marginBottom: '5%'}} className="item">
                         <header>
-                            <a href="https://studentsmeet.herokuapp.com"><img src={require('../../Images/headshot2.jpeg')} alt="" /></a>
+                            <a href="https://studentsmeet.herokuapp.com" target="_blank"><img className="voteImage" src={require('../../Images/studentsmeet.png')} alt="" /></a>
                             <h3>Students Meet</h3>
                         </header>
                         <p>This is a project about... ajfhald;hfjakls;hlk;dasfh fkdlhdfjalk;dsfkl;sdfhkl ;dsfanjdsf kl;fdcvjndksla;jfkl ;sdnfkml,sdaf; nklas;nfkl;</p>
 
-                        <a href="https://studentsmeet.herokuapp.com" class="button">Learn More</a>
+                        <Button href="https://studentsmeet.herokuapp.com" variant="contained" color="primary" target="_blank">Visit Site</Button>
                     </article>
             </section>
+            <Button href="https://www.github.com/nrking0" variant="contained" color="primary" target="_blank">View Source Code on GitHub</Button>
         </Element>
     )
 };
