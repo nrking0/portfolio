fe15ea1ff6ac7aad71e103487e3e3198905ac7db
Nick King <nrking0@gmail.com>
2020-09-30 23:19:49 -0500

    Added Descriptions

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -15,7 +15,7 @@ const Projects = (props) => {
                             <a href="https://voteuhigh.herokuapp.com" target="_blank"><img className="voteImage" src={require('../../Images/voteuhigh.png')} alt="" /></a>
                             <h3>Vote U-High</h3>
                         </header>
-                        <p>This is a project about... ajfhald;hfjakls;hlk;dasfh fkdlhdfjalk;dsfkl;sdfhkl ;dsfanjdsf kl;fdcvjndksla;jfkl ;sdnfkml,sdaf; nklas;nfkl;</p>
+                        <p>This is a website I created to run a mock election at my high school. I used Django as the framework for the front and back end and utilized a PostgreSQL database for all of the data. I used a user authentication system to ensure only real students could vote and that they could only vote once.</p>
                         <Button href="https://voteuhigh.herokuapp.com" variant="contained" color="primary" target="_blank">Visit Site</Button>
                         
                     </article>
@@ -25,7 +25,7 @@ const Projects = (props) => {
                             <a href="https://studentsmeet.herokuapp.com" target="_blank"><img className="voteImage" src={require('../../Images/studentsmeet.png')} alt="" /></a>
                             <h3>Students Meet</h3>
                         </header>
-                        <p>This is a project about... ajfhald;hfjakls;hlk;dasfh fkdlhdfjalk;dsfkl;sdfhkl ;dsfanjdsf kl;fdcvjndksla;jfkl ;sdnfkml,sdaf; nklas;nfkl;</p>
+                        <p>This is a PERN stack web application I created with Amit Sawhney for the 2020 HackIllinois Hackathon. It uses a custom made algorithm to match students with mentors in common areas. We also designed a custom backend with a PostgreSQL database and Express.js app for communication to store and present the data.</p>
 
                         <Button href="https://studentsmeet.herokuapp.com" variant="contained" color="primary" target="_blank">Visit Site</Button>
                     </article>
