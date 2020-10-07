8a00ebe9da0910685403009eb12c4018f8cda02d
Nick King <nrking0@gmail.com>
2020-10-06 23:24:02 -0500

    Final Touches

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -15,10 +15,10 @@ const Experience = (props) => {
             <Fade bottom>
                 <div className="experienceMain">
                     <div className="descrip">
-                        <h3>Throughout my time in high school, and now in college, I have participated in a wide variety of extra-cutticulars, volunteering, and work experiences. I always 
+                        <p>Throughout my time in high school, and now in college, I have participated in a wide variety of extra-cutticulars, volunteering, and work experiences. I always 
                             have loved trying new things, and that mantra has followed through to many of my extra-curriculars. While I do have a strong foundation with activities in Computer 
                             Science, I have always loved having experiences in a wide variety of disciplines. Whether it is working on a new project or serving ice cream, I always put my all 
-                            into whatever I do!</h3>
+                            into whatever I do!</p>
                     </div>
                     <div className="accordion">
                         <Accordion>
