d9e52507ec2108c7ea6af12220fd40fe424888da
Nick King <nrking0@gmail.com>
2021-03-10 16:18:23 -0600

    Small update to experience

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -39,6 +39,13 @@ const activities = [
     },
     {
         "type": "Work",
+        "title": 'Honors Introduction to Computer Science',
+        "role": 'Project Manager and Course Assistant',
+        "date": 'January 2021 – May 2021',
+        "description": 'For Spring semester 2021, I was selected to be a project manager for the honors section of introduction to computer science (CS 196). I am managing a small project group to build a full stack application that analyzes stock data. Additionally, I help out with the course by running office hours and helping students.'
+    },
+    {
+        "type": "Work",
         "title": 'Gene\'s Ice Cream',
         "role": 'Shift Lead',
         "date": 'March 2018 – August 2020',
@@ -147,118 +154,80 @@ const Experience = (props) => {
                         <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
                     </div>
                 ) : (
-                        <>
-                            {section === "Curriculars" ? (
-                                <div className="toggleBar">
-                                    <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
-                                    <Button style={{ margin: '1%' }} variant="contained" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
-                                    <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
-                                </div>
-                            ) : (
-                                    <>
-                                        {section === "Courses" ? (
-                                            <div className="toggleBar">
-                                                <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
-                                                <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
-                                                <Button style={{ margin: '1%' }} variant="contained" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
-                                            </div>
-                                        ) : (
-                                                <div className="toggleBar">
-                                                    <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
-                                                    <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
-                                                    <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
-                                                </div>
-                                            )}
-                                    </>
+                    <>
+                        {section === "Curriculars" ? (
+                            <div className="toggleBar">
+                                <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
+                                <Button style={{ margin: '1%' }} variant="contained" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
+                                <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
+                            </div>
+                        ) : (
+                            <>
+                                {section === "Courses" ? (
+                                    <div className="toggleBar">
+                                        <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
+                                        <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
+                                        <Button style={{ margin: '1%' }} variant="contained" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
+                                    </div>
+                                ) : (
+                                    <div className="toggleBar">
+                                        <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Work") }}>Work Experience</Button>
+                                        <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Curriculars") }}>Extra-Curriculars</Button>
+                                        <Button style={{ margin: '1%' }} variant="outlined" color="secondary" onClick={() => { handleClick("Courses") }}>Relevant Coursework</Button>
+                                    </div>
                                 )}
-                        </>
-                    )}
+                            </>
+                        )}
+                    </>
+                )}
                 <div className="experienceMain">
                     <div className="slideshow">
                         <Slideshow></Slideshow>
                     </div>
-                    
+
                     <div className="paperList">
-                         {activities.map((activity) => (
-                             <Fade collapse when={section === activity.type}>
-                                <div style={{marginBottom:'2%'}}>
-                                <Card className={classes.root} elevation={3}>
-                                    <CardContent>
-                                        <Typography variant="h6" component="h2">
-                                            {activity.title}
-                                        </Typography>
-                                        <Typography className={classes.pos} color="inherit">
-                                            {activity.role}
-                                        </Typography>
-                                        <Typography className={classes.pos} color="inherit">
-                                            {activity.date}
-                                        </Typography>
-                                        <Typography variant="body2" component="p">
-                                            {activity.description}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </div>
+                        {activities.map((activity) => (
+                            <Fade collapse when={section === activity.type}>
+                                <div style={{ marginBottom: '2%' }}>
+                                    <Card className={classes.root} elevation={3}>
+                                        <CardContent>
+                                            <Typography variant="h6" component="h2">
+                                                {activity.title}
+                                            </Typography>
+                                            <Typography className={classes.pos} color="inherit">
+                                                {activity.role}
+                                            </Typography>
+                                            <Typography className={classes.pos} color="inherit">
+                                                {activity.date}
+                                            </Typography>
+                                            <Typography variant="body2" component="p">
+                                                {activity.description}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </div>
                             </Fade>
-                        ))} 
+                        ))}
                     </div>
 
                     <div className="accordion">
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>Association for Computing Machinery</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Special Interest Group for Information Security Member <br />August 2020 – Present</h4>
-                                <p>Throughout ACM I have had many opportunities to learn more about the amazing world of Computer Science! Within the
-                                special interest group for information security I have learned a lot about best practices and vulnerabilities within our current technologies.
-                                Additionally, I have been able to work on projects with other students for things we care about. For example, this Fall I am working with a team within ACM to
-                                build an Android App that can help communicate important voter information to students and help them register to vote.</p>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>Marching Illini</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Trumpet Section Member<br />August 2020 – Present</h4>
-                                <p>In the Summer of 2020 I auditioned for, and eventually was accepted into, the Marching Illini. While our season looks a little different due to COVID-19, we still have many
-                                responsibilities. We learn a large repertoire, which we would normally play at each football game. Beyond that, we work on marching fundamentals, leadership development, and
-                                simply bonding more as a band.</p>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>Campus Honors Program Honors Student Council</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Academic Engagement Committee Member<br />August 2020 – Present</h4>
-                                <p>At the beginning of the 2020 school year I decided to join the Honors Student Council. Through the Academic Engagement Committee I have planned events to help bring students
-                                academic resources and help them meet professors. I have set up meet-and-greets and talks between students and professors and publicized the events to garner wider engagement.</p>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>Gene's Ice Cream</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Shift Lead<br />March 2018 – August 2020</h4>
-                                <p>Over the past three years, I worked at an ice cream shop while I was in high school. I worked across all roles in the stores, while maintaining an important focus on customer
-                                service. I eventually moved towards a leadership role, where I have helped manage shifts and lead different tasks during our work. I have counted the cash in the drawers
-                                at the end shifts, and I helped look into new items that we could sell at the store.</p>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>University High School Student Senate</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Student Body Vice President<br />August 2016 - May 2020</h4>
-                                <p>Throughout all four years in high school I was elected to my school's Student Senate. In my senior year I was elected as Student Body Vice President. I organized many events
-                                during my time in Senate including dances, fundraisers, and school spirit events. Additionally, in my senior year I became the coordinator of our biannual Red Cross blood
-                                drive. I helped organize the event and get as many donors as possible. We ended up getting more donors than the past two years combined my senior year!</p>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion>
-                            <MuiAccordionSummary expandIcon={<MdExpandMore />}>University High School Speech Team</MuiAccordionSummary>
-                            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
-                                <h4>Captain<br />August 2016 - May 2020</h4>
-                                <p>As a member of the Speech team I competed mainly in impromptu and extemporaneous speaking. I competed across the national circuit where I performed in a national final,
-                                two state finals, a tournament of champions final, and a tournament of champions semifinal. In my senior year, I worked to introduce the team culture to new members and
-                                bring more people into my events. During my senior year our team finished as a top twenty school in the nation at the national tournament.</p>
-                            </AccordionDetails>
-                        </Accordion>
+                        {activities.map((activity) => (
+                            <>
+                                {activity.type === "Courses" ? (
+                                    <></>
+                                ) : (
+                                    <Accordion>
+                                        <MuiAccordionSummary expandIcon={<MdExpandMore />}>{activity.title}</MuiAccordionSummary>
+                                        <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
+                                            <h4>{activity.role}<br />{activity.date}</h4>
+                                            <p>{activity.description}</p>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                )}
+                            </>
+                        ))}
                     </div>
+                    
                 </div>
             </Fade>
         </Element>
